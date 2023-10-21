0f30c366f0438613d70e8cbda90b47ebd24e2ae3
jason <jason.frey.jf@gmail.com>
2023-10-21 18:47:46 +0200

    g

---
+++ src/ch/frey/jason/abstractClasses/Scene.java
@@ -0,0 +1,13 @@
+package ch.frey.jason.abstractClasses;
+//TODO aluege was e Abstract Class isch / wieso abstractClass brucht wird
+public abstract class Scene {
+	
+	public Scene() {
+		
+	}
+	
+	public abstract void update(float dt);
+	
+	
+	
+}
